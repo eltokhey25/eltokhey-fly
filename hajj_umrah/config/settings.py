ef936939f92f9bb08f04a9fa3a36188dd9920720
eltokhey <eltokhey@2m3.tail6e1958.ts.net>
2026-09-16 07:12:53 +0300

    Fix STATIC_ROOT and STATICFILES_DIRS

--- hajj_umrah/config/settings.py
+++ hajj_umrah/config/settings.py
@@ -84,12 +84,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
